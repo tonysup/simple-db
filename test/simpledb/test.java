791834d57b250722d01141c1b6795c43e3c058cd
Tony <peng2251@163.com>
2017-11-17 17:43:41 +0800

    build

--- test/simpledb/test.java
+++ test/simpledb/test.java
@@ -13,7 +13,7 @@ public class test {
 
         // create the table, associate it with some_data_file.dat
         // and tell the catalog about the schema of this table.
-        HeapFile table1 = new HeapFile(new File("some_data_file.dat"), descriptor);
+        HeapFile table1 = new HeapFile(new File("E:\\info.dat"), descriptor);
         Database.getCatalog().addTable(table1, "test");
 
         // construct the query: we use a simple SeqScan, which spoonfeeds
